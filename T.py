2fa7e70fea59b493d857c86a414913142bb11308
quoc-trung-developver <66318023+quoc-trung-developver@users.noreply.github.com>
2020-06-10 20:25:26 +0700

    Update T.py

--- T.py
+++ T.py
@@ -1,1 +1,23 @@
-Print ("hellogit")
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+N = input (">>")
+I = 0
+while I == 0:
+    if N == "R":
+        f = input("file: ")
+        j = open (j,"r+")
+        i = f.read()
+        print (i)
+        f.close()
+    elif N == "E":
+        break
+    else:
+        print ("os")
+    N = input (">>")
+        

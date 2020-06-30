49cdac347a992617be6f41925ae3302ec2741da7
quoc-trung-developver <66318023+quoc-trung-developver@users.noreply.github.com>
2020-06-30 09:26:08 +0700

    Add files via upload

---
+++ i.py
@@ -0,0 +1,19 @@
+import time
+import os
+os.system ('toilet -f mono12 -F gay "hello"')
+os.system ("figlet 3")
+time.sleep (1)
+os.system ("figlet 2")
+time.sleep (1)
+os.system ("figlet 1")
+time.sleep (1)
+os.system("figlet This")
+time.sleep (0.5)
+os.system("figlet Is")
+time.sleep (0.5)
+os.system("figlet For")
+time.sleep (0.5)
+os.system("figlet You")
+time.sleep (1)
+os.system("cacafire")
+
